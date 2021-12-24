5871139f820b4c50cbbd500b4c80fde146911be1
Erwann <erwanndierick@gmail.com>
2021-12-24 14:40:07 +0100

    Ajout de la branche "Erwann"

---
+++ AA_Fil_Rouge/src/fr/epsi/test/TestRestaurant.java
@@ -0,0 +1,14 @@
+package fr.epsi.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class TestRestaurant {
+
+	@Test
+	public void test() {
+		Assert.assertEquals(150, 75 + 75);
+	}
+
+}
